f1cba93627ff53ae73f5b43711e8b266b1e0bb59
glai <glaiobido23@gmail.com>
2019-04-17 01:05:00 +0800

    - leadtype vuex setup

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -6,6 +6,7 @@ export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE'
 export const UPDATE_USER = 'UPDATE_USER'
 
-// leadtype.js
-export const FETCH_LEADTYPE_SUCCESS = '';
-export const UPDATE_LEADTYPE = '';
+// leadtypes.js
+export const FETCH_LEADTYPES_SUCCESS = 'FETCH_LEADTYPES_SUCCESS';
+export const FETCH_LEADTYPE_SUCCESS = 'FETCH_LEADTYPE_SUCCESS';
+export const UPDATE_LEADTYPE = 'UPDATE_LEADTYPE';
