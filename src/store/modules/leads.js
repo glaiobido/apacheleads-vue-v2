b818134a61ac04681314632e6bf6876665dc4cff
glai <glaiobido23@gmail.com>
2019-04-20 22:36:46 +0800

    - Vuex module for leads added

---
+++ src/store/modules/leads.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import * as types from '../mutation-types'
+
+// state
+export const state = {
+    leads: [],
+    lead: null,
+    id: 0
+}
+
+// getters
+export const getters = {
+    leads: state => state.leads,
+    lead: state => (id) => {
+        return state.leads.find(lead => lead.id == id)
+    }
+}
+
+// mutations
+export const mutations = {
+  [types.FETCH_LEAD_SUCCESS] (state, { leads }) {
+    state.leadtype = leadtype
+  }
+
+}
+
+// actions
+export const actions = {
+  
+    async fetchLeads ({ commit }) {
+        try {
+            const { data } = await axios.get('/leads');
+            commit(types.FETCH_LEAD_SUCCESS, { leads: data })
+        } catch (e) {
+        
+        }
+    }
+}
