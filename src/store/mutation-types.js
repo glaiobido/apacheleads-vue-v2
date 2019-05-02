87375daeb7c587e6f635673334ae1af9bd2a1386
glai <glaiobido23@gmail.com>
2019-05-03 00:39:02 +0800

    - mutation variables for users module

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -13,3 +13,8 @@ export const UPDATE_LEADTYPE = 'UPDATE_LEADTYPE';
 
 // leads.js
 export const FETCH_LEAD_SUCCESS = 'FETCH_LEAD_SUCCESS';
+
+// users.js
+export const FETCH_ALL_USERS = 'FETCH_ALL_USERS_SUCCESS';
+export const FETCH_ALL_ADMINS = 'FETCH_ALL_ADMINS';
+export const FETCH_ALL_CUSTOMERS = 'FETCH_ALL_ADMINS';
