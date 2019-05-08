cafe432b6d55bb0bd3171cfb0bbceadf05c14996
glai <glaiobido23@gmail.com>
2019-05-08 20:25:25 +0800

    - global plugins installed

--- src/plugins/argon-dashboard.js
+++ src/plugins/argon-dashboard.js
@@ -2,8 +2,11 @@ import "@/assets/vendor/nucleo/css/nucleo.css";
 import "@/assets/scss/argon.scss";
 import globalComponents from "./globalComponents";
 import globalDirectives from "./globalDirectives";
-import SidebarPlugin from "@/components/SidebarPlugin/index"
-import NotificationPlugin from "@/components/NotificationPlugin/index"
+import SidebarPlugin from "@/components/SidebarPlugin/index";
+import NotificationPlugin from "@/components/NotificationPlugin/index";
+import VeeValidate from 'vee-validate';
+import ElementUI from 'element-ui';
+
 
 export default {
   install(Vue) {
@@ -11,5 +14,7 @@ export default {
     Vue.use(globalDirectives);
     Vue.use(SidebarPlugin);
     Vue.use(NotificationPlugin);
+    Vue.use(VeeValidate);
+    Vue.use(ElementUI);
   }
 };
