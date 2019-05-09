10991e9e5143c42ff1023697d84360f281f86131
glai <glaiobido23@gmail.com>
2019-05-10 01:23:43 +0800

    - added vuex modules for countries states

--- src/store/modules/countries.js
+++ src/store/modules/countries.js
@@ -5,18 +5,28 @@ import * as types from '../mutation-types'
 export const state = {
     countries: [],
     country: null,
+    states: [],
+    country_state: null,
     id: 0
 }
 
 // getters
 export const getters = {
-    countries: state => state.countries
+    countries: state => state.countries,
+    states: state => state.states,
+    country: state => (code) => {
+        return state.countries.find(country => country.country_code == code);
+    }
 }
 
 // mutations
 export const mutations = {
   [types.FETCH_COUNTRIES] (state, { countries }) {
     state.countries = countries;
+  },
+
+  [types.FETCH_STATES] (state, { states }) {
+    state.states = states;
   }
 
 }
@@ -28,6 +38,15 @@ export const actions = {
         try {
             const { data } = await axios.get('/countries');
             commit(types.FETCH_COUNTRIES, { countries: data })
+        } catch (e) {
+        
+        }
+    },
+
+    async fetchStates({commit}, payload) {
+        try {
+            const { data } = await axios.get('/states', { params: payload });
+            commit(types.FETCH_STATES, { states: data });
         } catch (e) {
         
         }
