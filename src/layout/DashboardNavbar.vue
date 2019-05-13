f64716ac9f8bae3b20340ca2229687adc1441064
glai <glaiobido23@gmail.com>
2019-05-14 06:07:44 +0800

    -hidden profile link

--- src/layout/DashboardNavbar.vue
+++ src/layout/DashboardNavbar.vue
@@ -20,10 +20,10 @@
                         <div class=" dropdown-header noti-title">
                             <h6 class="text-overflow m-0">Welcome!</h6>
                         </div>
-                        <router-link to="/profile" class="dropdown-item">
+                        <!-- <router-link to="/profile" class="dropdown-item">
                             <i class="ni ni-single-02"></i>
                             <span>My profile</span>
-                        </router-link>
+                        </router-link> -->
                        
                        
                         <router-link to="/login" @click.native="logout" class="dropdown-item">
