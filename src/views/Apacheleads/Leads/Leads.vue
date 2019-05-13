83580d68d1c43bb3289e00aa2b224be245828074
glai <glaiobido23@gmail.com>
2019-05-13 15:16:24 +0800

    On leads page = format table table

--- src/views/Apacheleads/Leads/Leads.vue
+++ src/views/Apacheleads/Leads/Leads.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <page-spinner :fullPage="true" :isLoading="activateSpinner"></page-spinner>
         <base-header type="gradient-default" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
 
         <div class="container-fluid mt--9">
@@ -7,58 +8,69 @@
                 <div class="col">
                     <div class="card shadow page-table-card">
                         <div class="card-header border-0 bg-transparent">
-                            <div class="row align-items-center">
-                                <div class="col">
-                                <h3 class="mb-0 text-dark">
-                                    Leads
-                                </h3>
+                            <form @submit.prevent="fetchLeads">
+                                <div class="row align-items-center">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label class="form-control-label">Select Lead Type: </label>
+                                            <v-select :options="leadtypes" 
+                                                    :reduce="leadtype => leadtype.id"
+                                                    v-model="form.leadtype_id"
+                                                    class="required"
+                                                    label="name"  />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label class="form-control-label">Leads Imported Up To Date: </label>
+                                            <base-input addon-left-icon="ni ni-calendar-grid-58">
+                                                <flat-picker slot-scope="{focus, blur}"
+                                                            @on-open="focus"
+                                                            @on-close="blur"
+                                                            :config="{allowInput: true}"
+                                                            class="form-control datepicker"
+                                                            v-model="form.import_date">
+                                                </flat-picker>
+                                            </base-input>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group mb-0">
+                                            <label class="form-control-label"></label>
+                                            <base-button
+                                                nativeType="submit" 
+                                                type="default">
+                                                Get Leads
+                                            </base-button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col text-right">
-                                <!-- <base-button 
-                                        type="default"
-                                        @click="showAddModal = true" 
-                                        >Add Lead Type</base-button> -->
+                            </form>                            
+                        </div>
+
+                        <div class="card-body" >
+                            <div class="row">
+                                <div class="col">
+                                    <el-table
+                                        :data="leads"
+                                        style="width: 100%"
+                                        height="400"
+                                        empty-text="No Data Available">
+
+                                        <el-table-column
+                                            sortable
+                                            v-for="(field, index) in formatted_fields" :key="index"
+                                            :fixed="index==0"
+                                            props="id"
+                                            :label="field"
+                                            width="150">
+                                        </el-table-column>
+                                    </el-table>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="table-responsive">
-                            <base-table class="table align-items-center table-flush dark"
-                                        thead-classes="thead-light"
-                                        tbody-classes="list"
-                                        :data="leadtypes">
-
-                                <template slot="columns">
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Country</th>
-                                    <th></th>
-                                </template>
-
-                                <template slot-scope="{row}">
-                                    <td class="budget">
-                                        {{row.id}}
-                                    </td>
-                                    <td class="budget">
-                                        {{row.name}}
-                                    </td>
-                                    <td class="budget">
-                                        AU
-                                    </td>
-                                    <td class="budget">
-                                        <base-button
-                                            @click="triggerEditModal(row.id)" 
-                                            type="default" 
-                                            class="my--1">Edit</base-button>
-                                        <base-button type="warning" 
-                                                     outline 
-                                                     class="my--1" 
-                                                     @click="deleteLeadType(row.id)">Delete</base-button>
-                                    </td>
-                                </template>
-
-                            </base-table>
-                        </div>
+                           
+                        
 
                         <div class="card-footer d-flex justify-content-end bg-transparent">
                            
@@ -76,15 +88,20 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import { mapGetters } from 'vuex';
-  
+    import axios from 'axios';
+    import { mapGetters } from 'vuex';
+    import vSelect from 'vue-select';
+    import 'vue-select/dist/vue-select.css';
+    import flatPicker from "vue-flatpickr-component";
+    import "flatpickr/dist/flatpickr.css";
   
 
     export default {
-        name: 'tables',
+        
         components: {
-         
+            vSelect,
+            flatPicker
+
         },
 
         created() {
@@ -93,46 +110,45 @@
 
         data() {
             return {
-                showAddModal: false,
-                showEditModal: false,
-                id: 0
+                activateSpinner: false,
+                form: {
+                    leadtype_id: null,
+                    import_date: null
+                }
             }
         },
 
         computed: {
             ...mapGetters({
                 user: 'auth/user',
-                leadtypes: 'leadtypes/leadtypes'
-            })
+                leadtypes: 'leadtypes/leadtypes',
+                leadtype_id: 'leadtypes/leadtype_id',
+                leads: 'leads/leads'
+            }),
+
+            formatted_fields() {
+                if (this.leads.length > 0) {
+                    return Object.keys(this.leads[0]);
+                }
+
+                return [];
+            }
         },
 
         methods: {
-            triggerEditModal(lead_type_id) {
-                this.id = lead_type_id;
-                this.showEditModal = true;
-            },
-
-            async deleteLeadType(lead_type_id) {
-
-                this.$swal({
-                    title: 'Are you sure?',
-                    text: "You won't be able to revert this!",
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, delete it!'
-                }).then((result) => {
-                    if (result.value) {
-                        axios.delete(`/leadtypes/${lead_type_id}`).then(response => {
-                           this.$store.dispatch('leadtypes/fetchLeadTypes');
-                        });
-                        this.$swal(
-                            'Deleted!',
-                            'Your file has been deleted.',
-                            'success'
-                        )
-                    }
+            async fetchLeads() {
+                
+                let self = this;
+                console.log("submit get: ", this.form)
+                this.activateSpinner = true;
+                axios.get('/leads', {params: this.form}).then((response) => {
+                    let {data} = response;
+                    this.$store.dispatch('leads/setFetchedLeads', data);
+                    console.log("RESPONSE LEAD: ", data)
+                    self.activateSpinner = false;
+                })
+                .catch((e) => {
+                        
                 });
             }
         }
@@ -146,5 +162,10 @@
 
     .page-table-card {
         min-height: 70vh;
+    }
+
+    .vs__dropdown-toggle {
+        padding: 6px;
+        background-color: #fff;
     }
 </style>
